00955cf3aa72c6db455acad136b4c319920ad49f
Antoine Malpel <antoinemalpel@MacBook-Pro-de-Antoine.local>
2024-02-27 13:27:37 +0100

    db connection refacto, and using env values

--- controllers/note-controller.js
+++ controllers/note-controller.js
@@ -1,40 +1,31 @@
 import mysql from 'mysql2/promise';
 
+const dbConnection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
+console.log('connexion db réussie');
+
 export class NoteController {
   async listAll(req, res) {
     console.log('noteController should list them all');
-    const dbConnection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      database: 'notes_app'
-    });
-    console.log('connexion db réussie');
     const [results, fields] = await dbConnection.query('SELECT * FROM notes');
     res.send(results);
   }
 
   async create(req, res) {
-    const dbConnection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      database: 'notes_app'
-    });
-
     const newNote = {
       text: req.body.text
     };
-
     console.log('noteController create with text : ', newNote.text);
     const [results, fields] = await dbConnection.query('INSERT INTO notes (text) VALUE (?)', [newNote.text]);
-    res.json({message: "note added to"});
+    res.json({message: "note added", results: results});
   }
 
   async destroy(req, res) {
-    const dbConnection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      database: 'notes_app'
-    });
     const [results, fields] = await dbConnection.query('DELETE FROM notes WHERE id = ?', [req.params.id]);
     res.json({message: "note deleted",  results: results});
   }
