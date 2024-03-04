85d08ca43b135348916307294075b90d87e479d7
tangorri <tangorri@live.fr>
2024-03-04 22:05:55 +0100

    User can login, response is a JWT

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 
 import { Router } from 'express';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 import { dbQuery } from '../db.js';
 
@@ -20,6 +21,30 @@ router.post('/', async function (req, res) {
 
   const result = await dbQuery('INSERT INTO users (username, password) VALUES (?,?)', [username, hashedPassword]);
   res.json({ message: "user registered", result });
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const [users] = await dbQuery('SELECT * FROM users WHERE username = ?', [username]);
+    const user = users[0];
+    console.log('login, user: ', user);
+    if (!user) {
+      return res.status(401).json({ error: 'Authentication failed' });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Authentication failed' });
+    }
+    // const token = jwt.sign({ userId: user._id }, 'your-secret-key', {
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY, {
+      expiresIn: '1h',
+    });
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Login failed' });
+  }
 });
 
 export default router;
