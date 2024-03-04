4f7dd7d28776aed0c4267425956320f86416682d
tangorri <tangorri@live.fr>
2024-03-04 21:34:58 +0100

    user password is now hashed

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,25 @@
+// @note based on tutorial https://dvmhn07.medium.com/jwt-authentication-in-node-js-a-practical-guide-c8ab1b432a49
+
 import { Router } from 'express';
-var router = Router();
+import bcrypt from 'bcrypt';
 
 import { dbQuery } from '../db.js';
 
+const router = Router();
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/', async function(req, res) {
+// Register a User
+// In Rest that means post a new User
+router.post('/', async function (req, res) {
   const { username, password } = req.body;
-  const result = await dbQuery('INSERT INTO users (username, password) VALUES (?,?)', [username, password]);
-  res.json({message: "user registered", result });
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const result = await dbQuery('INSERT INTO users (username, password) VALUES (?,?)', [username, hashedPassword]);
+  res.json({ message: "user registered", result });
 });
 
 export default router;
