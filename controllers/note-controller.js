98fa7f3be0bb504de7ff1fa78c3e28fe3a3ab1e8
tangorri <tangorri@live.fr>
2024-03-04 21:00:14 +0100

    REFACTORED DB Layer

--- controllers/note-controller.js
+++ controllers/note-controller.js
@@ -1,18 +1,9 @@
-import mysql from 'mysql2/promise';
-
-const dbConnection = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT
-});
-console.log('connexion db réussie');
+import { dbQuery } from '../db.js';
 
 export class NoteController {
   async listAll(req, res) {
     console.log('noteController should list them all');
-    const [results, fields] = await dbConnection.query('SELECT * FROM notes');
+    const [results, fields] = await dbQuery('SELECT * FROM notes');
     res.send(results);
   }
 
@@ -21,17 +12,17 @@ export class NoteController {
       text: req.body.text
     };
     console.log('noteController create with text : ', newNote.text);
-    const [results, fields] = await dbConnection.query('INSERT INTO notes (text) VALUE (?)', [newNote.text]);
+    const [results, fields] = await dbQuery('INSERT INTO notes (text) VALUE (?)', [newNote.text]);
     res.json({message: "note added", results: results});
   }
 
   async update(req, res) {
-    const [results] = await dbConnection.query('UPDATE notes SET text = ? WHERE id= ?', [req.body.text, req.params.id]);
+    const [results] = await dbQuery('UPDATE notes SET text = ? WHERE id= ?', [req.body.text, req.params.id]);
     res.json({ message: "note updated", results: results});
   }
 
   async destroy(req, res) {
-    const [results, fields] = await dbConnection.query('DELETE FROM notes WHERE id = ?', [req.params.id]);
+    const [results, fields] = await dbQuery('DELETE FROM notes WHERE id = ?', [req.params.id]);
     res.json({message: "note deleted",  results: results});
   }
 }
