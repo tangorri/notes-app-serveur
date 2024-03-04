4359cc676ff8ad58b0d3bff7386c0d35b055a3c0
tangorri <tangorri@live.fr>
2024-03-04 21:22:15 +0100

    FEAT: user register (stupid&dangerous)

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 var router = Router();
 
+import { dbQuery } from '../db.js';
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/', async function(req, res) {
+  const { username, password } = req.body;
+  const result = await dbQuery('INSERT INTO users (username, password) VALUES (?,?)', [username, password]);
+  res.json({message: "user registered", result });
 });
 
 export default router;
