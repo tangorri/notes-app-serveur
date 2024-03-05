35d10cf7c0c8e15d9bddfd1438ebe67d38e97214
tangorri <tangorri@live.fr>
2024-03-05 12:11:56 +0100

    user register ok

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
-var router = Router();
+import { dbQuery } from '../db.js';
+
+const router = Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/', async function (req, res) {
+  console.log('register new user');
+  const {username, password } = req.body;
+  const [result] = await dbQuery("INSERT INTO users (username, password) VALUES (?, ?)", [username, password]);
+  res.json({message: "user created", result });
 });
 
 export default router;
